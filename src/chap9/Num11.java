7b7cde0271c15191d4e4903887dcf430709abf8c
Hawn <feel000617@gmail.com>
2022-10-07 01:23:25 +0900

    first commit

--- src/chap9/Num11.java
+++ src/chap9/Num11.java
@@ -6,26 +6,25 @@ import java.util.Scanner;
 //내가짠 두서없는 코드..
 public class Num11 {
     static String DashInsert(String s){
-        String[] numbers = s.split("");
-        int[] arr=new int[numbers.length];
+        String[] numbers = s.split("");     //입력받은 s를 ""기준으로 나누어서 numbers 배열에 저장
+        int[] arr=new int[numbers.length];      //numbers의 길이만큼 arr배열을 만든다.
         for(int j=0; j< numbers.length; j++){
-            arr[j]=Integer.parseInt(numbers[j]);
+            arr[j]=Integer.parseInt(numbers[j]);//for문을 돌면서 numbers의 원소들을 정수로 변환해 arr에 저장한다.
         }
-        ArrayList<String> newlist = new ArrayList<>();
-
-        for(int i=0; i< arr.length-1; i++){
-            newlist.add(Integer.toString(arr[i]));
-            if (arr[i]%2==0&&arr[i+1]%2==0){
-                newlist.add("*");
-            } else if (arr[i]%2==1&&arr[i+1]%2==1) {
-                newlist.add("-");
+        ArrayList<String> newlist = new ArrayList<>();//ArrayList newlist를 생성한다.
+        //arr i인덱스와 i+1인덱스를 비교
+        for(int i=0; i< arr.length-1; i++){ //arr의 길이-1만큼 for문을 돌린다.
+            newlist.add(Integer.toString(arr[i]));  //newlist에 i번째 arr를 추가한다.
+            if (arr[i]%2==0&&arr[i+1]%2==0){        //만약 i번째와 i+1번째(이어지는 두수) arr가 짝수면
+                newlist.add("*");                   //newlist에 '*'추가
+            } else if (arr[i]%2==1&&arr[i+1]%2==1) {//만약 i번째와 i+1번째(이어지는 두수) arr가 홀수면
+                newlist.add("-");                   //newlist에 '-'추가
             }
-        }if (arr[arr.length-1]%2==0&&arr[arr.length-1]%2==0) {
-            newlist.add(Integer.toString(arr[arr.length-1]));
-        } else if (arr[arr.length-1]%2==1&&arr[arr.length-1]%2==1) {
-            newlist.add(Integer.toString(arr[arr.length-1]));
         }
+        //arr의 마지막 인덱스추가
+        newlist.add(Integer.toString(arr[arr.length-1]));
         String num="";
+        //ArrayList 문자열로 변환
         for(String str:newlist){
             num+=str;
         }
--- src/chap9/Num12.java
+++ src/chap9/Num12.java
@@ -1,13 +1,70 @@
 package chap9;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
-
+//나의 조잡한 풀환
 public class Num12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input=sc.nextLine();
 
-        int cnt =0;
-        
+        //입력받은 String을 ""기준으로 쪼개서 alpha 배열에 넣기
+        String[] alpha = input.split("");
+        //alpha 배열을 list에 ArrayList로 저장
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(alpha));
+        ArrayList<String> list1 = new ArrayList<>();
+        //반복횟수 반복이 없는 경우 초기값 1
+        int cnt=1;
+        //list의 마지막 인덱스를 제외하고,반복여부 파악
+        for (int i=0; i< list.size()-1; i++){
+            if(list.get(i).equals(list.get(i+1))){
+                cnt+=1; //반복시 1더하기
+            }else{
+                list1.add(list.get(i)); //반복이 아닐시 list1에 i번째 인덱스 추가
+                list1.add(""+cnt);      //그 뒤에 반복 횟수 추가(""+)를 함으로써 문자열로 저장
+                cnt=1;      //초기값 설정
+            }
+        }
+        list1.add(list.get(list.size()-1)); //마지막 인덱스 추가
+        list1.add(""+cnt);      //반복횟수 추가(이전문자와 다르면 1, 같으면 그동안의 cnt값)
+        // String으로 변환
+        String result="";
+        for (String alphabet:list1){
+            result+=alphabet;
+        }
+        System.out.println(result);
     }
 }
+
+/*
+public class Num12 {
+    static String compressString(String data) {
+        String _c = "";  // data 문자열중 현재 진행중인 문자를 임시저장하기 위한 변수
+        int cnt = 0;  // 해당 문자가 몇 번 반복했는지 알 수 있는 카운트 변수
+        String result = "";
+        for (String c : data.split("")) {  // 입력받은 문자열 data에서 문자 하나씩 c에 대입
+            if (!c.equals(_c)) {  // 현재 진행중인 문자와 c가 같지 않은 경우, 즉 새로운 문자의 시작
+                _c = c;  // 현재 진행중인 문자와 같지 않으므로 현재진행문자는 c로 대입
+                if (cnt > 0) {  // 새로운 문자이므로 결과 문자열에 이전 문자의 카운트(있을 경우에만)에 해당하는 값을 더해 주어야 함
+                    result += "" + cnt;
+                }
+                result += c;  // 새로운 문자이므로 결과문자열에 새로운 문자를 더함
+                cnt = 1;  // 새로운 문자이므로 카운트는 1로 초기화
+
+            } else {
+                cnt += 1;  // 현재 진행중인 문자와 c가 같기 때문에 카운트가 증가됨
+            }
+        }
+        if (cnt > 0) {  // for 문을 벗어낫을때 이전 문자의 카운트는 최종적으로 한번 더해주어야함
+            result += "" + cnt;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String result = compressString("aaabbcccccca");
+        System.out.println(result);  // a3b2c6a1 출력
+    }
+}
+ */
